added training on fixed batch number, change the computation of metrics, added prints for dataset preparation and and minor changes on dataset class

adapter_entity_typing/datasets_classes/bertDatasets.py
@@ -3,16 +3,23 @@ from transformers import AutoTokenizer
 import time
 import torch
 import numpy as np
+from tqdm import tqdm
 
 
 class BertDataset(Dataset):
-    def __init__(self, sent, labels, label_number):
+    def __init__(self, sent, labels, label_number, tokenized_sent = [], attn_masks = []):
         self.sent = sent
         self.labels = labels
         self.label_number = label_number
         self.tokenizer = AutoTokenizer.from_pretrained("bert-base-uncased")
         self.max_length = self.compute_max_length()
-        self.tokenize()
+        
+        self.tokenized_sent, self.attn_masks = tokenized_sent, attn_masks
+        if not tokenized_sent and not attn_masks:
+            self.tokenize()
+        elif (not tokenized_sent and attn_masks) or (tokenized_sent and not attn_masks):
+            raise Exception('tokenized_sent and attn_masks have to be both empty or not')
+
 
     def compute_max_length(self):
         max_length = 0
@@ -27,13 +34,13 @@ class BertDataset(Dataset):
         print('... Tokenization started ...')
         print('... Maximum length : {} ...'.format(self.max_length))
         t = time.time()
-        self.tokenized_sent, self.attn_masks = [], []
         tok = self.tokenizer(self.sent, max_length = self.max_length, padding = 'max_length', truncation = True)
-        for i in range(len(self.sent)):
+        print('... Tokenized in {:.2f} seconds ...'.format(time.time() - t))
+        print('... Extracting tokenized sentences ...')
+        for i in tqdm(range(len(self.sent))):
             self.tokenized_sent.append(tok['input_ids'][i])
             self.attn_masks.append(tok['attention_mask'][i])
         # self.tokenized_sent = [self.tokenizer(s, max_length=80, truncation=True, padding="max_length")['input_ids'] for s in self.sent]
-        print('... Tokenized in {:.2f} seconds ...'.format(time.time() - t))
         
     def __getitem__(self, idx):
         return np.asarray(self.tokenized_sent[idx]), np.asarray(self.attn_masks[idx]), self.get_one_hot_from_idx(idx)
@@ -47,5 +54,3 @@ class BertDataset(Dataset):
 
     def __len__(self):
         return len(self.tokenized_sent)
-
-    